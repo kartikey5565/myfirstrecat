4bb4758fa8718ce7f0b72792505163a376f16764
kartikey5565 <18BET1068>
2023-04-21 12:40:37 +0530

    task 7 lets start coding

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import MyItem from "./Components/ExpenseItem";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>let's get started!</h2>
+      <MyItem></MyItem>
     </div>
+
   );
 }
 
