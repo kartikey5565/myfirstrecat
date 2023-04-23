7d3156b5a575a634ef96b61eca94ef565922cad7
kartikey5565 <18BET1068>
2023-04-23 13:46:01 +0530

    use state ttle and amount

--- src/Components/Expenses/ExpensesItem.js
+++ src/Components/Expenses/ExpensesItem.js
@@ -2,24 +2,27 @@ import "./ExpenseItem.css";
 import Card from "../UI/Card";
 import ExpenseDate from './ExpenseDate'
 import ExpenseDetails from './ExpenseDetails'
+import React, {useState} from "react";
 const ExpenseItem =(props)=> {
 
+  const [title, setTitle] = useState(props.title)
+  const [amount, setAmount] = useState(props.amount)
+
     const clickHandler = ()=>{
+      setTitle("Updated!")
       console.log("clicked")
     }
-    const deleteHandler = (e)=>{
-      console.log(e.target.parentNode)
-      const r = document.querySelector("#root").firstChild
-      r.removeChild(e.target.parentNode)
+    const expenseHandler = ()=>{
+      setAmount("$100")
     }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date = {props.date}/>
-      <ExpenseDetails title = {props.title} amount = {props.amount}/>
+      <ExpenseDetails title = {title} amount = {amount}/>
       <button onClick={clickHandler}>Change Title</button>
-      <button onClick={deleteHandler}>Delete</button>
+      <button onClick={expenseHandler}>Change Expense</button>
     </Card>
   );
 }
-export default ExpenseItem;
+export default ExpenseItem
