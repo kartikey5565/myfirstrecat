e91bec80a35bcca5f7d3009d4078d2b1e3cf40f5
kartikey5565 <18BET1068>
2023-04-24 13:11:20 +0530

    implement filter

--- src/App.js
+++ src/App.js
@@ -5,34 +5,35 @@ import ExpenseFilter from "./components/Expenses/ExpenseFilter";
 import { useState } from "react";
 //import newExpense from "./components/newExpense/newExpense";
 const dummyData = [
-  // {
-  //   id: "e1",
-  //   title: "Toilet Paper",
-  //   amount: 94.12,
-  //   date: new Date(2020, 7, 14)
-  // },
-  // {
-  //   id: "e2",
-  //   title: "New TV",
-  //   amount: 799.49,
-  //   date: new Date(2021, 2, 12)
-  // },
-  // {
-  //   id: "e3",
-  //   title: "Car Insurance",
-  //   amount: 294.67,
-  //   date: new Date(2021, 2, 28)
-  // },
-  // {
-  //   id: "e4",
-  //   title: "New Desk (Wooden)",
-  //   amount: 450,
-  //   date: new Date(2021, 5, 12)
-  // }
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14)
+  },
+  {
+    id: "e2",
+    title: "New TV",
+    amount: 799.49,
+    date: new Date(2021, 2, 12)
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28)
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12)
+  }
 ];
 const App = () => {
   console.log("Rendered")
   const [expenses, updateExpenses] = useState(dummyData);
+  const [selectedYear, setSelectedYear] = useState('2020')
 
   const newExpenseHandler = data =>{
     console.log(data)
@@ -41,21 +42,21 @@ const App = () => {
     })
     console.log(expenses)
   }
-  const yearSelectHandler = data =>{
-
-    console.log(data)
+  const yearSelectHandler = data =>{ 
+    setSelectedYear(data)
   }
 
-  return (
+  
 
-    <AllExpenses>
-
+  return (<div>
       <NewExpense onNewExpense = {newExpenseHandler}/>
-      
       <ExpenseFilter onYearSelect ={yearSelectHandler}/>
       { expenses.map(expense => <MyItem key = {expense.id} title = {expense.title} amount= {expense.amount} date={expense.date}/>)}
+      <ExpenseFilter selected={selectedYear} onYearSelect ={yearSelectHandler}/>
+      <AllExpenses items = {expenses} date = {selectedYear}/>
+      </div>
 
-    </AllExpenses>
   );
 }
+
 export default App;
--- src/Components/Expense/AllExpenses.js
+++ src/Components/Expense/AllExpenses.js
@@ -1,10 +1,28 @@
-import './AllExpenses.css'
+import "./AllExpenses.css";
+import ExpenseItem from "./ExpenseItem";
+const AllExpenses = (props) => {
+  // const classes = 'allexpenses ' + props.className
+  var filteredArr = props.items.filter(
+    (e) => e.date.getFullYear().toString() === props.date
+  );
+  let expensesContent = <p>No Expenses</p>
+  if (filteredArr.length>0){
+    expensesContent = filteredArr.map((element) => (
+        <ExpenseItem
+          key={element.id}
+          title={element.title}
+          amount={element.amount}
+          date={element.date}
+        />
+      ))
+  }
+  return (
+    <div>
+      {expensesContent}
+    </div>
+  );
+};
 
-const AllExpense = (props)=> {
-    const classes = 'allexpenses ' + props.className
-    return (<div className= {classes}>
-                {props.children}
-    </div>);
-}
 
-export default AllExpense
+
+export default AllExpenses;
